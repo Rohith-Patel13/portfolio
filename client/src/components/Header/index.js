224a1fb29b81fd1a780e89750611178a21704a4f
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-03-31 16:17:08 +0530

    hover mouse color

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { MyContext } from '../MyContext/index'
 
 import portfolioIcon from '../../images/portfolio-icon.jpg'
@@ -8,17 +8,46 @@ import moon from '../../images/black-moon.png'
 import "./index.css"
 
 const Header = () => {
+  const aboutElementRef= useRef(null)
+  const servicesElementRef= useRef(null)
+  const skillsElementRef= useRef(null)
+  const portfolioElementRef= useRef(null)
+
   const {sectionRef,aboutRef,skillsRef,projectsRef,servicesRef}=useContext(MyContext)
   
 
   const handleClicked =(particularRef)=>{
     // console.log(particularRef.current)
-    const headerHeight = document.querySelector('.header-bg').offsetHeight;
+    const headerHeight = document.querySelector('.header-bg').offsetHeight; // offsetHeight is a property in JavaScript that returns the height of an element, including its vertical padding, vertical scrollbar (if present), and borders, but does not include margin.
+    // console.log(headerHeight,"headerHeight")
     if(particularRef.current){
       window.scrollTo({
-        top: particularRef.current.offsetTop - headerHeight,
-        behavior: "smooth"
+        top: particularRef.current.offsetTop - headerHeight,// adjusting the scroll position to ensure that the target element is visible below the header.
+        behavior: "smooth" // scrolling should be animated smoothly.
       });
+    }
+  }
+
+  // useEffect(()=>{
+  //   const headerHeightValue = document.querySelector('.header-bg').offsetHeight;
+  //   console.log(headerHeightValue,"headerHeightValue")
+  // })
+
+  const handleOnHover=(particularElementRef)=>{
+    // console.log(particularElementRef.current,"handleOnHover")
+    if(particularElementRef.current){
+      particularElementRef.current.style.fontWeight="bold"
+      particularElementRef.current.style.fontSize="20px"
+      particularElementRef.current.style.color="#6D54F1"
+    }
+  }
+
+  const handleOnLeaveHover=(particularElementRef)=>{
+    // console.log(particularElementRef.current,"handleOnLeaveHover")
+    if(particularElementRef.current){
+      particularElementRef.current.style.fontWeight="normal"
+      particularElementRef.current.style.fontSize="inherit"
+      particularElementRef.current.style.color="inherit"
     }
   }
 
@@ -28,13 +57,32 @@ const Header = () => {
         <img src={portfolioIcon} className='icon' alt="portfolioIcon" onClick={()=>handleClicked(sectionRef)} />
         <div className='nav-elements-bg'>
           <img src={moon} className='moon nav-el' alt='moon' />
-          <p className='about-nav-el nav-el' onClick={()=>handleClicked(aboutRef)}>About</p>
-          <p className='services-nav-el nav-el' onClick={()=>handleClicked(servicesRef)}>Services</p>
-          <p className='skills-nav-el nav-el' onClick={()=>handleClicked(skillsRef)}>Skills</p>
-          <p className='portfolio-nav-el nav-el' onClick={()=>handleClicked(projectsRef)}>Portfolio</p>
-          <p className='resume-nav-el nav-el'>Resume</p>
-          <p className='portfolio-nav-el nav-el'>Testimonials</p>
-          <p className='portfolio-nav-el nav-el'>Contact</p>
+          <p className='about-nav-el nav-el'
+           ref={aboutElementRef}
+           onMouseEnter={()=>handleOnHover(aboutElementRef)}
+           onMouseLeave={()=>handleOnLeaveHover(aboutElementRef)}
+           onClick={()=>handleClicked(aboutRef)}>About</p>
+          <p className='services-nav-el nav-el'
+           ref={servicesElementRef}
+           onMouseEnter={()=>handleOnHover(servicesElementRef)}
+           onMouseLeave={()=>handleOnLeaveHover(servicesElementRef)}
+           onClick={()=>handleClicked(servicesRef)}>Services</p>
+          <p className='skills-nav-el nav-el'
+           ref={skillsElementRef}
+           onMouseEnter={()=>handleOnHover(skillsElementRef)}
+           onMouseLeave={()=>handleOnLeaveHover(skillsElementRef)}
+           onClick={()=>handleClicked(skillsRef)}>Skills</p>
+          <p className='portfolio-nav-el nav-el'
+           ref={portfolioElementRef}
+           onMouseEnter={()=>handleOnHover(portfolioElementRef)}
+           onMouseLeave={()=>handleOnLeaveHover(portfolioElementRef)}
+           onClick={()=>handleClicked(projectsRef)}>Portfolio</p>
+          <p className='resume-nav-el nav-el'
+          >Resume</p>
+          <p className='portfolio-nav-el nav-el'
+          >Testimonials</p>
+          <p className='portfolio-nav-el nav-el'
+          >Contact</p>
         </div>
       </div>
     </nav>
